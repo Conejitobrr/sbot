4eaeecb75f23b2d9253f2e7eb4d8b44494b91c37
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2026-04-28 15:31:38 -0500

    Update disable.js

--- plugins/disable.js
+++ plugins/disable.js
@@ -10,6 +10,7 @@ module.exports = {
       sock,
       msg,
       remoteJid,
+      sender,
       args,
       fromGroup,
       isAdmin,
@@ -24,24 +25,27 @@ module.exports = {
       }, { quoted: msg })
     }
 
-    // BOT puede desactivarse en grupos o privado
+    // BOT (grupo o privado)
     if (feature === 'bot') {
-      if (!isAdmin && !isOwner) {
-        return sock.sendMessage(remoteJid, {
-          text: '❌ Solo admins/owner pueden usar este comando.'
-        }, { quoted: msg })
-      }
+      if (fromGroup) {
+        if (!isAdmin && !isOwner) {
+          return sock.sendMessage(remoteJid, {
+            text: '❌ Solo admins/owner pueden usar este comando.'
+          }, { quoted: msg })
+        }
 
-      await db.setChat(remoteJid, {
-        botEnabled: false
-      })
+        await db.setGroupSetting(remoteJid, 'bot', false)
+
+      } else {
+        await db.setUserSetting(sender, 'bot', false)
+      }
 
       return sock.sendMessage(remoteJid, {
         text: '✅ *bot* desactivado.'
       }, { quoted: msg })
     }
 
-    // Resto solo grupos
+    // Otras funciones solo en grupos
     if (!fromGroup) {
       return sock.sendMessage(remoteJid, {
         text: '❌ Este comando solo funciona en grupos.'
@@ -62,9 +66,7 @@ module.exports = {
       }, { quoted: msg })
     }
 
-    await db.setGroup(remoteJid, {
-      [feature]: false
-    })
+    await db.setGroupSetting(remoteJid, feature, false)
 
     await sock.sendMessage(remoteJid, {
       text: `✅ *${feature}* desactivado.`
